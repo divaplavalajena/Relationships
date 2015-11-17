2bc09e335e37f7ab5eacb5e2872c61ed877385c6
Jena Grafton <JenaLGrafton@gmail.com>
2015-11-17 09:53:43 -0600

    Relationships module beginning

--- Relationships/ViewController.swift
+++ Relationships/ViewController.swift
@@ -7,19 +7,127 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
+    
+    var context: NSManagedObjectContext!;
+    
+    var instructors: [Instructor] = [];
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        loadData();
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func loadData() {
+        
+        let request = NSFetchRequest(entityName: "Instructor");
+        
+        instructors = try! context.executeFetchRequest(request) as! [Instructor];
+        
+    }
+    
+    // use this function to save an instructor the first time you run the project
+    func SaveInstructor() {
+        let inst = NSEntityDescription.insertNewObjectForEntityForName("Instructor", inManagedObjectContext: context) as! Instructor;
+        inst.name = "Andrei";
+        inst.lastName = "Davis";
+        
 
+        try! context.save()
+
+        
+    }
+    
+    func saveCourse(title: String) {
+    
+    }
+
+    
+    @IBAction func displayInstructors(sender: AnyObject) {
+        
+        let request = NSFetchRequest(entityName: "Instructor");
+        
+        instructors = try! context.executeFetchRequest(request) as! [Instructor];
+        
+        
+        for instructor in instructors {
+            
+            print("Instructor Name: \(instructor.name!) \(instructor.lastName!)");
+            
+            for course in instructor.course! {
+                
+                let title = course.title as String?;
+                
+                print("Instructor's Courses: \(title!)");
+                
+            }
+        }
+        
+    }
+    
+    @IBAction func addCourse(sender: AnyObject) {
+        
+        let alert = UIAlertController(title: "New Student", message: "Add student name", preferredStyle: .Alert);
+        
+        let save = UIAlertAction(title: "Save", style: .Default) { (alertAction: UIAlertAction) -> Void in
+            let textField = alert.textFields![0] ;
+            self.saveCourse(textField.text!);
+        }
+        
+        let cancel = UIAlertAction(title: "Cancel", style: .Default) { (alertAction: UIAlertAction) -> Void in
+        }
+        
+        alert.addTextFieldWithConfigurationHandler { (textField: UITextField!) -> Void in
+        }
+        
+        alert.addAction(save);
+        alert.addAction(cancel);
+        
+        presentViewController(alert, animated: true, completion: nil);
+        
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
